e291a6aa1d7ecb5ed4fbac1630913fe84f0be6ad
ndom91 <yo@ndo.dev>
2021-01-14 23:57:36 +0100

    add: write cli arg

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import serverlessMysql from "serverless-mysql";
+import serverlessMysql from "serverless-mysql"
+
+const write = process.argv[2] === 'write'
 
 const mysql = serverlessMysql({
   config: {
@@ -26,38 +28,40 @@ const writeDays = async (days, email) => {
   const readQuery = `SELECT id, name, resturlaubVorjahr, jahresurlaubInsgesamt, jahresUrlaubAusgegeben, restjahresurlaubInsgesamt, beantragt, resturlaubJAHR FROM vacations WHERE email LIKE '${email}' AND disabled LIKE 0 AND approved LIKE 2 AND type LIKE 'vacation' ORDER BY submitted_datetime DESC LIMIT 1`
   const result = await mysql.query(readQuery)
 
-	if (result[0]) {
-		let daysLastYear = result[0].resturlaubVorjahr
-		let daysEarned = result[0].jahresurlaubInsgesamt
-		let daysSpent = parseFloat(result[0].jahresUrlaubAusgegeben)
-		let daysAvailable = result[0].restjahresurlaubInsgesamt
-		let daysRequested = result[0].beantragt
-		let daysLeft = result[0].resturlaubJAHR
-		
-		console.log('')
-		console.log(email)
-		console.log(daysLastYear, daysEarned, daysSpent, daysAvailable, daysRequested, daysLeft)
+  if (result[0]) {
+    let { 
+      resturlaubVorjahr: daysLastYear, 
+      jahresurlaubInsgesamt: daysEarned, 
+      jahresUrlaubAusgegeben: daysSpent, 
+      restjahresurlaubInsgesamt: daysAvailable,
+      beantragt: daysRequested,
+      resturlaubJAHR: daysLeft
+    } = result[0]
 
-		daysLastYear = daysLeft
-		daysEarned = days
-		daysSpent = 0
-		daysAvailable = days + daysLeft
-		daysRequested = 0
-		daysLeft = daysAvailable
+    console.log('')
+    console.log(email)
+    console.log(daysLastYear, daysEarned, daysSpent, daysAvailable, daysRequested, daysLeft)
 
-		console.log(daysLastYear, daysEarned, daysSpent, daysAvailable, daysRequested, daysLeft)
+    daysLastYear = daysLeft
+    daysEarned = days
+    daysSpent = 0
+    daysAvailable = days + daysLeft
+    daysRequested = 0
+    daysLeft = daysAvailable
 
-     // DEBUG
-     console.log(`${email} - had ${daysLastYear} - earned ${daysEarned} - now das ${daysAvailable} days`)
+    console.log(daysLastYear, daysEarned, daysSpent, daysAvailable, daysRequested, daysLeft)
 
-		// if (email === 'ndomino@newtelco.de') {
-		// const writeQuery = `INSERT INTO vacations (name, email, resturlaubVorjahr, jahresurlaubInsgesamt, jahresUrlaubAusgegeben, restjahresurlaubInsgesamt, beantragt, resturlaubJAHR, type, note, submitted_datetime, submitted_by, approved, approval_datetime, disabled) VALUES ('${result[0].name}', '${email}', ${daysLastYear}, ${daysEarned}, ${daysSpent}, ${daysAvailable}, ${daysRequested}, ${daysLeft}, 'vacation', 'System Update for 2021', '${new Date().toISOString().replace('T', ' ').split('Z')[0]}', 'ndomino', 2, '${new Date().toISOString().replace('T', ' ').split('Z')[0]}', 0);`
-		const writeResult = await mysql.query(writeQuery)
-		if (writeResult) {
-			console.log('Write Success...')
-		}
-		// }
-	}
+    // DEBUG
+    console.log(`HAD ${daysLastYear} + EARNED ${daysEarned} = HAS ${daysAvailable} days`)
+
+    if (write) {
+      const writeQuery = `INSERT INTO vacations (name, email, resturlaubVorjahr, jahresurlaubInsgesamt, jahresUrlaubAusgegeben, restjahresurlaubInsgesamt, beantragt, resturlaubJAHR, type, note, submitted_datetime, submitted_by, approved, approval_datetime, disabled) VALUES ('${result[0].name}', '${email}', ${daysLastYear}, ${daysEarned}, ${daysSpent}, ${daysAvailable}, ${daysRequested}, ${daysLeft}, 'vacation', 'System Update for 2021', '${new Date().toISOString().replace('T', ' ').split('Z')[0]}', 'ndomino', 2, '${new Date().toISOString().replace('T', ' ').split('Z')[0]}', 0);`
+      const writeResult = await mysql.query(writeQuery)
+      if (writeResult) {
+        console.log('Write Success...')
+      }
+    }
+  }
 }
 
 const getDaysEarned = years => {
@@ -86,7 +90,7 @@ joinDates.map(async user => {
 
   const daysEarned = getDaysEarned(yearsService)
 
-	await writeDays(daysEarned, user.email)
+  await writeDays(daysEarned, user.email)
 
 })
 
